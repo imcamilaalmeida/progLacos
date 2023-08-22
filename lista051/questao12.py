ea7740edb4f405fc3f0999122f17d2a7da1d0bef
Camila Almeida <camilaalma2008@gmail.com>
2023-08-22 11:42:41 -0300

    Todas questões resolvidas (menos a 15)

--- lista051/questao12.py
+++ lista051/questao12.py
@@ -5,6 +5,27 @@ de números deve parar quando o usuário digitar o número -1, e este número -1
 como maior, nem como menor, e nem na contagem da média
 '''
 
-a = int(input("Digite um número inteiro: "))
+a = float(input("Digite um número (-1 encerra o programa): "))
+b = a
+c = a
+d = 0
+soma = 0
 
 while (a != -1):
+    d = d + 1
+    soma = soma + a
+    if (b < a):
+        b = a
+
+    if (c > a):
+        c = a
+
+    a = float(input("Digite outro número (-1 encerra o programa): "))
+if (b == -1):
+    print("Você inseriu -1 na primeira resposta. \n PROGRAMA ENCERRADO!")
+
+else:
+    print(f"Maior valor inserido: {b:.0f}")
+    print(f"Menor valor inserido: {c:.0f}")
+    print(f"Média dos valores inseridos: {soma/d}")
+    print(f"Total de respostas: {d}")
--- lista051/questao15.py
+++ lista051/questao15.py
@@ -3,3 +3,12 @@ Desenvolver um programa que apresente a série de Fibonacci até o décimo quint
 formada pela sequência 1,1,2,3,5,8,13,21,34, ... etc. Ela se caracteriza pela soma de um termo posterior com seu
 anterior subsequente
 '''
+
+a = 0
+b = 1
+
+
+while (a <= 610):
+    c = a + b
+    print(f"O número da sequência Fibonacci é {c}")
+    
